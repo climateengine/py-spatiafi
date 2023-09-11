dfb6dd85318498862f88524d1b4ac5fb031d3a80
Douglas Anderson <hockeybuggy@gmail.com>
2023-09-11 11:45:06 -0300

    Remove `pytest-cov`
    We would get warnings when trying to measure tests coverage and wouldn't
    get particually useful feedback. This commit removes the measurement of
    test coverage. This is something we can add back someday if we consider
    it to be a valuable signal.

--- scripts/test.sh
+++ scripts/test.sh
@@ -4,10 +4,4 @@ set -eux -o pipefail
 # cd to the directory one higher than this script
 cd "$(dirname "${BASH_SOURCE[0]}")/.." || exit 1
 
-# Get python pkg name from the src dir
-python_pkg_slug=$(find src/ -maxdepth 1 -type d | sed -n '2p' | sed 's/^src\///')
-
-
-pytest \
-	--cov="$python_pkg_slug" \
-	--cov-fail-under=0
+pytest
