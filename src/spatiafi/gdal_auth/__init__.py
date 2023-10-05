591329ff6d8448ed8e964fb8f5a8b5fb65ee1d56
Bennett Kanuka <bennett@climateengine.com>
2023-10-05 18:03:12 -0400

    allow user functions to be called directly

--- src/spatiafi/gdal_auth/__init__.py
+++ src/spatiafi/gdal_auth/__init__.py
@@ -1,2 +1,7 @@
 from .cli import cli
-from .gdal_auth import get_credentials, get_gdal_env_vars
+from .gdal_auth import (
+    get_credentials,
+    get_gdal_env_vars,
+    get_user_auth_env_vars,
+    get_user_credentials,
+)
